0d99f94fc1f5f00df4c25c4fc871d2b5adfd4e28
dencypravin <106505390+dencypravin@users.noreply.github.com>
2025-06-25 10:47:58 +0530

    task1.cpp

--- task1.cpp
+++ task1.cpp
@@ -4,25 +4,25 @@
 #include <condition_variable>
 std::mutex mtx;
 std::condition_variable cv;
-bool is_even_turn = true;  // Start with even
+bool is_even_turn = true; 
 
 void print_even() {
     for (int i = 0; i <= 10; i += 2) {
         std::unique_lock<std::mutex> lock(mtx);
-        cv.wait(lock, [] { return is_even_turn; });  // Wait for even turn
+        cv.wait(lock, [] { return is_even_turn; });
         std::cout << "Even: " << i << std::endl;
         is_even_turn = false;
-        cv.notify_one();  // Notify odd thread
+        cv.notify_one();  
     }
 }
 
 void print_odd() {
     for (int i = 1; i < 10; i += 2) {
         std::unique_lock<std::mutex> lock(mtx);
-        cv.wait(lock, [] { return !is_even_turn; });  // Wait for odd turn
+        cv.wait(lock, [] { return !is_even_turn; });  
         std::cout << "Odd: " << i << std::endl;
         is_even_turn = true;
-        cv.notify_one();  // Notify even thread
+        cv.notify_one(); 
     }
 }
 
